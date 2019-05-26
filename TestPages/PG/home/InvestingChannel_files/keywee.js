696aafc1cf5e8dc307bc2b75537554b5cf4077a5
Prabhatg1214 <gk.prabhat@gmail.com>
2019-05-26 18:16:11 +0000

    Delete all

--- TestPages/PG/home/InvestingChannel_files/keywee.js
+++
@@ -1,6 +0,0 @@
-!function(e,n,o,w,a,i,c){e[a]||(e.GlobalSnowplowNamespace=e.GlobalSnowplowNamespace||[],e.GlobalSnowplowNamespace.push(a),e[a]=function(){(e[a].q=e[a].q||[]).push(arguments)},e[a].q=e[a].q||[],i=n.createElement(o),c=n.getElementsByTagName(o)[0],i.async=1,i.src=w,c.parentNode.insertBefore(i,c))}(window,document,"script","//dc8xl0ndzn2cb.cloudfront.net/sp.js","snowplow"),function(){function e(){var e=function(e,n,o,w){var a,i
-if(o){var c=new Date
-c.setTime(c.getTime()+24*o*60*60*1e3),a="; expires="+c.toGMTString()}else a=""
-i=e+"="+n+a+"; path=/;",w&&(i=i+" Domain="+w),document.cookie=i},o="kwp_4",w="keywee."+o,a=new RegExp(o+"=([^&]*)","i").exec(window.location.href),i=a&&unescape(a[1])||""
-""!=i&&e(w,i,30,n),window.snowplow("setUserIdFromCookie",w)}var n=".investingchannel.com"
-window.snowplow("newTracker","cf","pixel.keywee.co",{appId:"7"}),e(),window.snowplow("enableActivityTracking",30,30),window.snowplow("trackPageView")}()
